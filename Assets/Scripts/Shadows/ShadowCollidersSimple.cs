86aaba2cf1285a36b185042e1ebfc1d673d0cb38
Christian Thomsen <christian@fundayfactory.com>
2021-09-12 11:22:43 +0200

    Small cleanup after moving to simple colliders

--- Assets/Scripts/Shadows/ShadowCollidersSimple.cs
+++ Assets/Scripts/Shadows/ShadowCollidersSimple.cs
@@ -5,6 +5,100 @@ using UnityEngine;
 using System.Linq;
 using MergeSort;
 
+
+public class Vector2Pair
+{
+    public Vector2 v1;
+    public Vector2 v2;
+    public Vector2 delta;
+
+    public Vector2Pair(Vector2 v1, Vector2 v2)
+    {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.delta = v2 - v1;
+    }
+}
+
+public class ShapeCollider
+{
+    public GameObject parentObject;
+    public BoxCollider2D[] colliders;
+    public Rigidbody2D[] colliderBodies;
+    private LightData lightData;
+
+    private List<Vector2Pair> colliderVecs = new List<Vector2Pair>();
+
+    public ShapeCollider(GameObject parent)
+    {
+        colliders = new BoxCollider2D[4];
+        colliderBodies = new Rigidbody2D[4];
+
+        for (var i = 0; i < 4; i++)
+        {
+            GameObject col = new GameObject("col" + i);
+            Rigidbody2D body = col.AddComponent<Rigidbody2D>();
+            body.bodyType = RigidbodyType2D.Kinematic;
+            body.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+            body.sleepMode = RigidbodySleepMode2D.NeverSleep;
+            body.interpolation = RigidbodyInterpolation2D.Interpolate;
+            colliderBodies[i] = body;
+
+            colliders[i] = col.AddComponent<BoxCollider2D>();
+            colliders[i].size = new Vector2(10f, 0.1f);
+            col.transform.SetParent(parent.transform);
+        }
+
+        SetEnabled(false);
+    }
+
+    public void SetEnabled(bool enabled)
+    {
+        bool wasDisabled = false;
+        for (var i = 0; i < colliders.Length; i++)
+        {
+            wasDisabled = wasDisabled ? wasDisabled : !colliders[i].gameObject.activeInHierarchy;
+            colliders[i].gameObject.SetActive(enabled);
+
+        }
+
+        if (wasDisabled)
+            UpdateColliders(true);
+    }
+
+    public void SetColliderPoints(int index, Vector2 p1, Vector2 p2)
+    {
+        if (colliderVecs.Count <= index)
+            colliderVecs.Add(new Vector2Pair(p1, p2));
+        else
+            colliderVecs[index] = new Vector2Pair(p1, p2);
+    }
+
+    public void UpdateColliders(bool instant = false)
+    {
+        //Scale and position properly
+        for (var i = 0; i < colliderVecs.Count; i++)
+        {
+            Vector2Pair vecs = colliderVecs[i];
+            Vector2 newPosition = vecs.v1 + vecs.delta * 0.5f;
+            Quaternion newRotation = Quaternion.Euler(0f, 0f, Mathf.Atan2(vecs.delta.y, vecs.delta.x) * 180f / Mathf.PI);
+
+            if (instant)
+            {
+                colliderBodies[i].position = newPosition;
+                colliderBodies[i].rotation = newRotation.eulerAngles.z;
+            }
+            else
+            {
+                colliderBodies[i].MovePosition(newPosition);
+                colliderBodies[i].MoveRotation(newRotation);
+            }
+
+            colliders[i].size = new Vector2(vecs.delta.magnitude, colliders[i].size.y);
+        }
+    }
+}
+
 public class ShadowCollidersSimple : MonoBehaviour
 {
     private LightData[] lights;
--- Assets/Scripts/Shadows/ShadowSystem.cs
+++ Assets/Scripts/Shadows/ShadowSystem.cs
@@ -10,15 +10,13 @@ public class ShadowSystem : MonoBehaviour
 
     public ShadowRenderer shadowRenderer;
     public ComputeBuffers computeBuffers;
-    public ShadowColliders shadowColliders;
     public Color shadowColor;
 
     private EdgeVertex[] shadowEdgeVertices = new EdgeVertex[ComputeBuffers.EDGEVERTEX_BUFFER_SIZE];
 
-    public static Vector2Int textureResolution = new Vector2Int(1920, 1080) / 1;
+    public static Vector2Int textureResolution = new Vector2Int(1920, 1080);
 
     private int shadowComputeShaderKI;
-    private int moveTexPixelsKI;
 
     void Start()
     {
@@ -30,12 +28,6 @@ public class ShadowSystem : MonoBehaviour
         computeShader.SetInts("resolution", new int[] { textureResolution.x, textureResolution.y });
 
         shadowComputeShaderKI = computeShader.FindKernel("ShadowComputeShader");
-
-        moveTexPixelsKI = computeShader.FindKernel("MoveTexturePixels");
-        computeShader.SetTexture(moveTexPixelsKI, "shadowTexture", shadowRenderer.shadowTexture);
-        computeShader.SetTexture(moveTexPixelsKI, "lightTexture", shadowRenderer.lightTexture);
-
-        //StartCoroutine(DelayedUpdate());
     }
 
     private void SetupAndDispatchComputeShader()
@@ -52,100 +44,8 @@ public class ShadowSystem : MonoBehaviour
         computeShader.Dispatch(shadowComputeShaderKI, textureResolution.x / 32, textureResolution.y / 30, 1);
     }
 
-    private IEnumerator DelayedUpdate()
-    {
-        yield return new WaitForEndOfFrame();
-
-        SetupAndDispatchComputeShader();
-
-        //Copy shadow edge vertices from shader to shadowEdgeVertices array
-        ComputeBuffer edgeVertexBuffer = computeBuffers.GetEdgeVertexBuffer();
-        int vertexCount = computeBuffers.GetAppendBufferCount(edgeVertexBuffer);
-        int powTwoCount = (int)Mathf.Pow(2, Mathf.Ceil(Mathf.Log(vertexCount) / Mathf.Log(2)));
-
-        Debug.Log("Count: " + vertexCount + ", powTwoCount =" + powTwoCount);
-
-        BitonicMergeSort sort = new BitonicMergeSort(vertexSortComputeShader);
-        DisposableBuffer<uint> keys = new DisposableBuffer<uint>(powTwoCount);
-
-        sort.Init(keys.Buffer);
-        sort.SortEdgeVertices(keys.Buffer, edgeVertexBuffer);
-        keys.Download();
-
-        edgeVertexBuffer.GetData(shadowEdgeVertices);
-
-        var failed = false;
-        var lastVal = shadowEdgeVertices[keys.Data[0]].angleToShapeCenter;
-        for (var i = 0; i < powTwoCount; i++)
-        {
-            var val = shadowEdgeVertices[keys.Data[i]].angleToShapeCenter;
-            if (val > lastVal)
-            {
-                failed = true;
-                Debug.LogErrorFormat("Unexpected Key {0} at {1}", val, i);
-            }
-
-            lastVal = val;
-        }
-
-        Debug.LogFormat("Sort Test Result = {0}", (failed ? "Wrong" : "Correct"));
-
-        //shadowColliders.UpdateColliders(shadowEdgeVertices, keys, vertexCount);
-    }
-
     private void Update()
     {
-        /*for (var i = 0; i < shadowEdgeVertices.Length; i++)
-        {
-            shadowEdgeVertices[i].shapeIndex = -1;
-            shadowEdgeVertices[i].angleToShapeCenter = -99;
-            shadowEdgeVertices[i].position = Vector2.zero;
-        }
-
-        computeBuffers.GetEdgeVertexBuffer().SetData(shadowEdgeVertices);*/
-
         SetupAndDispatchComputeShader();
-
-        //Copy shadow edge vertices from shader to shadowEdgeVertices array
-        /*ComputeBuffer edgeVertexBuffer = computeBuffers.GetEdgeVertexBuffer();
-        int vertexCount = computeBuffers.GetAppendBufferCount(edgeVertexBuffer);
-        int powTwoCount = (int)Mathf.Pow(2, Mathf.Ceil(Mathf.Log(vertexCount) / Mathf.Log(2)));
-
-        if (vertexCount == 0)
-            return;
-
-        BitonicMergeSort sort = new BitonicMergeSort(vertexSortComputeShader);
-        DisposableBuffer<uint> keys = new DisposableBuffer<uint>(powTwoCount);
-
-        sort.Init(keys.Buffer);
-        sort.SortEdgeVertices(keys.Buffer, edgeVertexBuffer);
-        keys.Download();
-
-        edgeVertexBuffer.GetData(shadowEdgeVertices);
-
-        var failed = false;
-        var lastVal = shadowEdgeVertices[keys.Data[0]].angleToShapeCenter;
-        for (var i = 0; i < vertexCount; i++)
-        {
-            var val = shadowEdgeVertices[keys.Data[i]].angleToShapeCenter;
-            if (val > lastVal)
-            {
-                failed = true;
-                Debug.LogErrorFormat("Unexpected Key {0} at {1}", val, i);
-            }
-
-            lastVal = val;
-        }
-
-        if (failed)
-            Debug.LogWarningFormat("Sort Test Result = {0}", (failed ? "Wrong" : "Correct"));
-
-        //shadowColliders.Create(shadowEdgeVertices, keys.Data, vertexCount);
-        keys.Dispose();*/
-    }
-
-    public void Move(int direction)
-    {
-        computeShader.Dispatch(moveTexPixelsKI, textureResolution.x / 32, textureResolution.y / 30, 1);
     }
 }
